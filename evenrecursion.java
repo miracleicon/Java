ddd49708c5780c2680ae5a17a83a90696305ca0c
Mayank Shrivastava <146957986+miracleicon@users.noreply.github.com>
2023-11-01 09:51:28 +0530

    Create evenrecursion.java

---
+++ evenrecursion.java
@@ -0,0 +1,20 @@
+public class EvenNumberFinder {
+    public static boolean isEven(int number) {
+        if (number == 0) {
+            return true;
+        }
+        if (number == 1) {
+            return false;
+        }
+        return isEven(number - 2);
+    }
+    public static void main(String[] args) {
+        int number = 10; 
+        boolean result = isEven(number);
+        if (result) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+    }
+}
